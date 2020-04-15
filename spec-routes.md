6d209e6e48ff7bfb12578fd390622a3b0533bac1
YokaiEmporer <taranrishit1234@gmail.com>
2020-04-15 10:52:53 +0530

    added actuators

---
+++ spec-routes.md
@@ -0,0 +1,4 @@
+hal browser :- localhost:8080
+actuators :- localhost:8080/actuator
+swagger docs :localhost:8080/v2/api-docs
+swagger gui:
--- src/main/java/com/YE/rest/practice/practice/SwaggerConfig.java
+++ src/main/java/com/YE/rest/practice/practice/SwaggerConfig.java
@@ -11,9 +11,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+//	public static final Contact DEFAULT_CONTACT = new Contact("Taran", "Rishit", "Undru");
+//	  public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("hmm title", "hmm Documentation", "1.0", "urn:tos",
+//	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
+
 	@Bean
 	public Docket api()
 	{
+		
 		return new Docket(DocumentationType.SWAGGER_2);
+//				.apiInfo(DEFAULT_API_INFO);
 	}
 }
