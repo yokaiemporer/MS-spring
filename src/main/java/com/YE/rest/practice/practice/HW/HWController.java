5f6c7a9d21a817265b227d5591c4849871ecc88f
YokaiEmporer <taranrishit1234@gmail.com>
2020-04-15 07:10:44 +0530

    Hateoas

--- src/main/java/com/YE/rest/practice/practice/HW/HWController.java
+++ src/main/java/com/YE/rest/practice/practice/HW/HWController.java
@@ -1,13 +1,21 @@
 package com.YE.rest.practice.practice.HW;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HWController {
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+	
 	@RequestMapping(path="/hello",method=RequestMethod.GET)
 	public String helloworld()
 	{
@@ -22,6 +30,11 @@ public class HWController {
 	public HWBean helloworldbean(@PathVariable String name)
 	{
 		return new HWBean("Hello World"+name);
+	}
+	@GetMapping(path="/hweighteen")
+	public String helloWorldI18n(@RequestHeader(name="Accept-Language",required=false) Locale locale)
+	{
+		return messageSource.getMessage("good.morning.message", null,locale);
 	}
 	
 	
--- src/main/java/com/YE/rest/practice/practice/PracticeApplication.java
+++ src/main/java/com/YE/rest/practice/practice/PracticeApplication.java
@@ -1,7 +1,12 @@
 package com.YE.rest.practice.practice;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class PracticeApplication {
@@ -9,5 +14,18 @@ public class PracticeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
-
+	@Bean
+	public SessionLocaleResolver localeResolver()
+	{
+		SessionLocaleResolver localeresolver=new SessionLocaleResolver();
+		localeresolver.setDefaultLocale(Locale.US);
+		return localeresolver;
+	}
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource()
+	{
+		ResourceBundleMessageSource msgsrc=new ResourceBundleMessageSource();
+		msgsrc.setBasename("messages");
+		return msgsrc;
+	}
 }
